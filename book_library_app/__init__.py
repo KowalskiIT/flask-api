1f6a0df105c9dece4ef625b0a713258f85ad0160
KowalskiIT <k.now224@gmail.com>
2023-10-23 16:12:41 +0200

    DB URI + Authors model

--- book_library_app/__init__.py
+++ book_library_app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.sql import text
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+
 from book_library_app import authors
+from book_library_app import models
