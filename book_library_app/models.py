386e5d1dddaddee72275dda4ea8a7c09324d2872
KowalskiIT <k.now224@gmail.com>
2023-11-14 19:26:49 +0100

    comparasion filter method for GET authors

--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,8 +1,13 @@
+import re
 from book_library_app import db
+from sqlalchemy.orm.attributes import InstrumentedAttribute
+from sqlalchemy.sql.expression import BinaryExpression
 from datetime import datetime
 from flask_sqlalchemy.query import Query
 from marshmallow import Schema, fields, validate, validates, ValidationError
 from werkzeug.datastructures import ImmutableDict
+
+COMPARISON_OPERATORS_RE = re.compile(r'(.*)\[(gte|gt|lte|lt)\]')
 
 
 class Author(db.Model):
@@ -36,9 +41,24 @@ class Author(db.Model):
         return query
 
     @staticmethod
+    def get_filter_argument(column_name: InstrumentedAttribute, value: str, operator: str) -> BinaryExpression:
+        operator_mapping = {
+            '==': column_name == value,
+            'gte': column_name >= value,
+            'gt': column_name > value,
+            'lte': column_name <= value,
+            'lt': column_name < value
+        }
+        return operator_mapping[operator]
+
+    @staticmethod
     def apply_filter(query: Query, params: ImmutableDict) -> Query:
         for param, value in params.items():
             if param not in {'fields', 'sort'}:
+                operator = '=='
+                match = COMPARISON_OPERATORS_RE.match(param)
+                if match is not None:
+                    param, operator = match.groups()
                 column_attr = getattr(Author, param, None)
                 if column_attr is not None:
                     if param == 'birth_date':
@@ -46,7 +66,8 @@ class Author(db.Model):
                             value = datetime.strptime(value, '%d-%m-%Y').date()
                         except ValueError:
                             continue
-                    query = query.filter(column_attr == value)
+                    filter_argument = Author.get_filter_argument(column_attr, value, operator)
+                    query = query.filter(filter_argument)
         return query
 
 
